e2f8f13a3716f7fab2f25839058de422456ec2b8
beyzaaltuntas <baltuntas@etu.edu.tr>
2020-04-20 06:59:21 +0300

    bugs fixed

--- Api/Channel3Service.cs
+++ Api/Channel3Service.cs
@@ -13,7 +13,7 @@ namespace Recommender.Api
         private static readonly HttpClient client = new HttpClient();
         public async Task<string[]> getPeopleAlsoSearchForAsync(string workOfArt)
         {
-            var response = await client.GetStringAsync($"https://app.zenserp.com/api/v2/search?apikey=bf36c2c0-8263-11ea-9b0f-6f1e52de7167&q={workOfArt}&lr=lang_tr&hl=tr&location=Turkey&gl=tr");
+            var response = await client.GetStringAsync($"https://app.zenserp.com/api/v2/search?apikey=33bf6160-82ae-11ea-b331-c350760a9587&q={workOfArt}&lr=lang_tr&hl=tr&location=Turkey&gl=tr");
             var x = JObject.Parse(response);
             string[] result;
             if (x["knowledge_graph"][0]["people_also_search_for"] != null)
